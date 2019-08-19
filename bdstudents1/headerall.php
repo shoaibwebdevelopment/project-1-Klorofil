de935ca39ee140d91320bd6c3f9093714383f433
Shoaib Ahned <shoaib1971@outlook.com>
2019-08-19 13:02:06 +0600

    modified file page-profile.html and a new file register.html addes in modified_project

---
+++ bdstudents1/headerall.php
@@ -0,0 +1,61 @@
+
+<?php
+
+
+?>
+
+
+<meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+  <title>bdstudents</title>
+
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600,700|Open+Sans:300,400,600,700" rel="stylesheet">
+  <link href="css/font-awesome.min.css" rel="stylesheet">
+
+  <link href="css/magnific-popup.css" rel="stylesheet">
+
+  <!-- Bootstrap -->
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="css/magnific-popup.css"> 
+
+  <link href="css/owl.carousel.min.css" rel="stylesheet">
+  <link href="css/owl.theme.default.min.css" rel="stylesheet">
+   <!-- Main-CSS -->
+  <link href="css/style.css" rel="stylesheet">
+
+  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+</head>
+<body>
+
+	
+	<!-- ============START: HEADER============= -->
+	<header>
+		<div class="header">
+			<div class="container">
+				
+				<div class="row">
+					<div class="col-md-12 col-sm-12 col-xs-12">
+						
+						<h3>শিখো, অনুশীলন করো এবং সফল হও !!!</h3>
+						<div class ="logoheader">
+							<img src = "images/logo4.png"  />
+						</div>
+	
+						<form class ="search-form">
+							<input type="text" size ="30" placeholder="টাইপ করুন যা  খুঁজতে চান ">
+							<button style= "color:red" >খোঁজন </button>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+	</header>
+	
---
+++ bdstudents1/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  
+
+
+
+  <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <!-- Include all compiled plugins (below), or include individual files as needed -->
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/jquery.magnific-popup.min.js"></script>
+  <script src="js/main.js"></script>
+</body>
+</html>
