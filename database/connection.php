3e5627d9cd397d9fc2cd08b74ed20920652989db
Shoaib Ahned <shoaib1971@outlook.com>
2019-08-20 20:16:43 +0600

    a file register.html deleted and two folder database where connection.php another folder layout where css-layout.php added also a modifed file register.php added in modified_project

---
+++ database/connection.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = " ";
+$dbname = "university_blog";
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+if($conn){
+
+    echo " ";
+}
+
+?>
--- register.php
+++ register.php
@@ -1,26 +1,108 @@
+
+<?php
+
+ include 'database/connection.php';
+
+ 
+ $error_name = '';
+ $error_email = '';
+ $error_password  = '';
+ $error_confirm_password  = '';
+ $error_mobile = '';
+ $error_university = '';
+ $length = '';
+ $confirmPassword = '';
+ $pwd = '';
+ $email_check = '';
+ $email_exist = '';
+
+
+
+if(isset($_POST['login'])){
+	$name = mysqli_escape_string($conn, $_POST['name']);
+	$email = mysqli_escape_string($conn, $_POST['email']);
+	$password = mysqli_escape_string($conn, $_POST['password']);
+	$confirmPassword = mysqli_escape_string($conn, $_POST['confirmPassword']);
+	$mobile = mysqli_escape_string($conn, $_POST['mobile']);
+	$university = mysqli_escape_string($conn, $_POST['university']);
+	$email_exist = "SELECT email FROM user WHERE email = '$email'";
+	$query = mysqli_query($conn, $email_exist);
+
+	if(mysqli_num_rows($query)>0){
+
+		$email_check = "Your Email Is Alredy Exist";
+	}
+	
+
+      if(empty($name)){
+
+		$error_name = "Name is required.";
+	  }
+
+	  else if(strlen($name)<5){
+
+		$length = "Length must be greater than four letter !";
+	  }
+	 
+
+	  else if(empty($email)){
+
+		$error_email = "email is required.";
+	  }
+
+	  else if(empty($password)){
+
+		$error_password = "password is required.";
+	  }
+
+	  else if(empty($confirmPassword)){
+
+		$error_confirm_password = "confirm password is required.";
+	  }
+	
+
+
+
+	  else if(empty($mobile)){
+
+
+		$error_mobile = "mobile number is required.";
+	  }
+
+	  else if(empty($university)){
+
+		$error_university = "university name is required.";
+	  }
+	 
+
+	
+
+	  else if($password!= $confirmPassword){
+
+		$pwd = "Your confirm password does not match !";
+	  }
+
+	  else {
+		  $password = md5($password);
+		  $v_key = md5(time().$email); // Encrypted value
+		  echo "<script> alert('$v_key')</script>";
+	  }
+
+	 
+	 
+
+}
+
+?>
+
+
+
 <!doctype html>
 <html lang="en" class="fullscreen-bg">
+<?php
+ include 'layouts/css/css-layout.php'
 
-<head>
-	<title>Registration</title>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
-	<!-- VENDOR CSS -->
-	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
-	<link rel="stylesheet" href="assets/vendor/font-awesome/css/font-awesome.min.css">
-	<link rel="stylesheet" href="assets/vendor/linearicons/style.css">
-	<!-- MAIN CSS -->
-	<link rel="stylesheet" href="assets/css/main.css">
-	<!-- FOR DEMO PURPOSES ONLY. You should remove this in your project -->
-	<link rel="stylesheet" href="assets/css/demo.css">
-	<!-- GOOGLE FONTS -->
-	<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700" rel="stylesheet">
-	<!-- ICONS -->
-	<link rel="apple-touch-icon" sizes="76x76" href="assets/img/apple-icon.png">
-	<link rel="icon" type="image/png" sizes="96x96" href="assets/img/favicon.png">
-</head>
-
+ ?>
 <body>
 	<!-- WRAPPER -->
 	<div id="wrapper">
@@ -33,39 +115,52 @@
 								<div class="logo text-center"><img src="assets/img/logo-dark.png" alt="Klorofil Logo">
 								</div>
 								<p class="lead">Register to your account</p>
+		
+		
 							</div>
 							<form class="form-auth-small" action="" method="POST">
 								<div class="form-group">
 									<label for="name" class="control-label sr-only">Name</label>
-									<input type="text" class="form-control" placeholder="Type Your Name" required
-										autofocus id="name">
+									<input type="text" class="form-control" placeholder="Type Your Name" name ="name" autofocus id="name">
+									<span class ="text-danger"><?= $error_name ?> <?= $length ?> </span>
 
 								</div>
 								<div class="form-group">
 									<label for="email" class="control-label sr-only">Email</label>
-									<input type="password" class="form-control" name="password"
-										placeholder="Type Your Email" required id="password">
+									<input type="email" class="form-control" name="email"
+										placeholder="Type Your Email"  id="email">
+										<span class ="text-danger"><?= $error_email ?> </span>
+
 								</div>
 
 								<div class="form-group">
 									<label for="password" class="control-label sr-only">Password</label>
 									<input type="password" class="form-control" name="password"
-										placeholder="Type Your password" required id="password">
+										placeholder="Type Your password"  id="password">
+										<span class ="text-danger"><?= $error_password ?> </span>
+
 								</div>
 								<div class="form-group">
 									<label for="confirmpassword" class="control-label sr-only">Confirm Password</label>
-									<input type="password" class="form-control" name="password"
-										placeholder="Type Your Confirm Password" required id="password">
+									<input type="password" class="form-control" name="confirmPassword"
+										placeholder="Type Your Confirm Password"  id="password">
+										<span class ="text-danger"><?= $error_confirm_password ?>  <?= $pwd; ?></span>
+
 								</div>
 								<div class="form-group">
 									<label for="mobile" class="control-label sr-only">Mobile</label>
 									<input type="mobile" class="form-control" name="mobile"
-										placeholder="Type Your Mobile Number" required id="mobile">
+										placeholder="Type Your Mobile Number"  id="mobile">
+										<span class ="text-danger bg-success"><?= $error_mobile ?> </span>
+
+										
 								</div>
 								<div class="form-group">
 										<label for="university" class="control-label sr-only">University Name</label>
 										<input type="text" class="form-control" name="university"
-											placeholder="Type Your University Name" required id="university">
+											placeholder="Type Your University Name"  id="university">
+											<span class ="text-danger"><?= $error_university ?> </span>
+
 									</div>
 								
 
@@ -76,7 +171,7 @@
 										<span>Remember me</span>
 									</label>
 								</div>
-								<button type="submit" class="btn btn-primary btn-lg btn-block">LOGIN</button>
+								<button type="submit" class="btn btn-primary btn-lg btn-block" name ="login">LOGIN</button>
 								<div class="bottom">
 									<span class="helper-text"><i class="fa fa-lock"></i> <a href="">Forgot
 											password?</a></span>
